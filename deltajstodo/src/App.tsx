41ea56c8a59efa138bba58df52b1d1310bc202aa
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-11 13:47:47 +0330

    fix : some change

---
+++ deltajstodo/src/App.tsx
@@ -0,0 +1,12 @@
+import "./dist/output.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-balance">سلام به همه</h1>
+      <div className="text-lime-300 text-gray-secondary">Hello World</div>
+    </>
+  );
+}
+
+export default App;
