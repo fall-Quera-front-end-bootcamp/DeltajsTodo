a1e36646ccb543e56ab1a11bde7a1d73011669ef
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-11 14:39:41 +0330

    build : add eslint and prettier config

--- deltajstodo/src/App.tsx
+++ deltajstodo/src/App.tsx
@@ -1,12 +1,17 @@
-import "./dist/output.css";
+// eslint-disable-next-line @typescript-eslint/semi
+import './dist/output.css'
+import React from 'react'
 
-function App() {
+// eslint-disable-next-line @typescript-eslint/space-before-function-paren
+function App(): JSX.Element {
   return (
     <>
       <h1 className="text-balance">سلام به همه</h1>
-      <div className="text-lime-300 text-gray-secondary">Hello World</div>
+      <div className="text-lime-300 text-gray-secondary p-2 mr-2 items-center  flex w-1 h-2">
+        Hello World
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
