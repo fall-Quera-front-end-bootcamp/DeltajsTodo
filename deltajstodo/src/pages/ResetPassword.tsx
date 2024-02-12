bfacde3b710dfb216ebb7c159c1a96b4e8dc210c
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-13 00:21:48 +0330

    build : ResetPassWrod page

--- deltajstodo/src/pages/ResetPassword.tsx
+++ deltajstodo/src/pages/ResetPassword.tsx
@@ -1,4 +1,8 @@
 import { type FunctionComponent } from 'react'
+import Layout from '../components/layout/Layout'
+import AuthCard from '../components/Card/AuthCard'
+import FormButton from '../components/Buttons/FormButton'
+import Input from '../components/Input/Input'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ResetPasswordProps {}
@@ -6,9 +10,29 @@ interface ResetPasswordProps {}
 const ResetPassword: FunctionComponent<ResetPasswordProps> = () => {
   return (
     <>
-      <div>
-        <p>Reset...</p>
-      </div>
+      <Layout
+        buttonText={'ورود'}
+        linkText={'قبلا ثبت‌نام کرده‌ای؟'}
+        linkSrc={'/api/auth/resetpassword'}
+      >
+        <AuthCard className="flex flex-col gap-8">
+          <h1 className="text-center font-yekan text-[32px] font-extrabold leading-[45.09px]">
+            تغییر رمز عبور
+          </h1>
+          <div className="flex flex-col gap-4">
+            <Input name="رمز عبور جدید را وارد کنید" type="text" />
+            <Input name="رمز عبور" type="password" />
+            <div className="">
+              <FormButton
+                className=""
+                text="اعمال تغییرات"
+                color="bg-brand-primary"
+                onClickFunction={() => {}}
+              />
+            </div>
+          </div>
+        </AuthCard>
+      </Layout>
     </>
   )
 }
