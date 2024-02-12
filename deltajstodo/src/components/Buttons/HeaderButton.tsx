bd837991286ff30682b4b6ddf357257db1b23003
behradahp <beh.afsharipor@gmail.com>
2024-02-12 23:40:00 +0330

    final components Edit

--- deltajstodo/src/components/Buttons/HeaderButton.tsx
+++ deltajstodo/src/components/Buttons/HeaderButton.tsx
@@ -1,20 +1,19 @@
 import '../../dist/output.css'
-import { NavLink } from 'react-router-dom'
 
-function HeaderButton({
-  text,
-  src,
-}: {
+interface HeaderButtonProps {
   text: string
-  src: string
-}): JSX.Element {
+  onClickFunction?: any
+}
+
+function HeaderButton({ text, ...props }: HeaderButtonProps): JSX.Element {
   return (
     <button
       className={
         'bg-brand-primary text-[white] font-yekan font-extrabold text-[14px] leading-[19.73px] rounded-[6px] p-[10px] w-[95px] h-[40px] mr-[6px]'
       }
+      onClick={props.onClickFunction}
     >
-      <NavLink to={src}>{text}</NavLink>
+      {text}
     </button>
   )
 }
