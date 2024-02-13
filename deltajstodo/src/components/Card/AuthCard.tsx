3976c33717119eaa9226b2a9c59247300e0947fe
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-13 18:20:53 +0330

    fix  : authcard classname

--- deltajstodo/src/components/Card/AuthCard.tsx
+++ deltajstodo/src/components/Card/AuthCard.tsx
@@ -1,6 +1,6 @@
 import '../../dist/output.css'
 
-function AuthCard ({ children }: { children: any }): JSX.Element {
+function AuthCard({ children }: { children: any }): JSX.Element {
   return (
     <div className="flex w-[100%] flex-col gap-4 rounded-[20px] bg-[white] p-6 shadow-card sm:w-[640px]">
       {children}
