7e6b5edf32d9dd3b52291be40a01c11ed121657e
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-16 18:20:09 +0330

    fix confilct

--- deltajstodo/src/pages/Register.tsx
+++ deltajstodo/src/pages/Register.tsx
@@ -9,7 +9,7 @@ import Checkbox from '../components/Checkbox'
 import { FormProvider, useForm } from 'react-hook-form'
 import TermsConditions from '../components/Modals/TermsConditions'
 import Button from '../components/Buttons/Button'
-import { AnimatePresence,motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface RegisterProps {}
