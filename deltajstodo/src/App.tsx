d84d0bdb3a5478ec590cb214128de90a838fdee6
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2024-02-17 00:25:00 +0330

    start bar not finish yet

--- deltajstodo/src/App.tsx
+++ deltajstodo/src/App.tsx
@@ -5,20 +5,22 @@ import Register from './pages/Register'
 import ForgetPassword from './pages/ForgetPassword'
 import ResetPassword from './pages/ResetPassword'
 import NotFound from './pages/NotFound'
+import QueraTaskManager from './components/SideBars/QueraTaskManager'
 
 // eslint-disable-next-line @typescript-eslint/space-before-function-paren
 function App(): JSX.Element {
   return (
-    <>
-      <Routes>
-        <Route path="api/auth/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
-        <Route path="api/auth/register" element={<Register />} />
-        <Route path="api/auth/forgetPassword" element={<ForgetPassword />} />
-        <Route path="api/auth/resetPassword" element={<ResetPassword />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <QueraTaskManager/>
+    // <>
+    //   <Routes>
+    //     <Route path="api/auth/login" element={<Login />} />
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="api/auth/register" element={<Register />} />
+    //     <Route path="api/auth/forgetPassword" element={<ForgetPassword />} />
+    //     <Route path="api/auth/resetPassword" element={<ResetPassword />} />
+    //     <Route path="*" element={<NotFound />} />
+    //   </Routes>
+    // </>
   )
 }
 
