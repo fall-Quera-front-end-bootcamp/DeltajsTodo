dde404abaf2354a417bc3c81b4f09f065706b10c
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-17 22:30:34 +0330

    build : start building calendar component

---
+++ deltajstodo/src/components/Calendar/Calendar.tsx
@@ -0,0 +1,9 @@
+const Calendar = (): JSX.Element => {
+  return (
+    <div className="absolute left-1/2 top-1/2 translate-x-1/2 translate-y-1/2 bg-white">
+      <div className="">Calendar</div>
+    </div>
+  )
+}
+
+export default Calendar
