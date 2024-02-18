dd4cb3754a9da0ab5f35cf1a584178ebb2e7f369
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-18 12:19:56 +0330

    fix : name of component

--- deltajstodo/src/components/SideBars/SidebarPrimary.tsx
+++ deltajstodo/src/components/SideBars/SidebarPrimary.tsx
@@ -1,22 +1,22 @@
-import {useState} from 'react'
-import ArrowDownIconSvg from '../Svg/ArrowDownIconSvg';
-import SearchIconSvg from '../Svg/SearchIconSvg';
-import AddSecondaryIconSvg from '../Svg/AddSecondaryIconSvg';
-import LogoutIconSvg from '../Svg/LogoutIconSvg';
-import LightModeSwitchIconSvg from '../Svg/LightModeSwitchIconSvg';
-import DarkModeSwitchIconSvg from '../Svg/DarkModeSwitchIconSvg';
+import { useState } from 'react'
+import ArrowDownIconSvg from '../Svg/ArrowDownIconSvg'
+import SearchIconSvg from '../Svg/SearchIconSvg'
+import AddSecondaryIconSvg from '../Svg/AddSecondaryIconSvg'
+import LogoutIconSvg from '../Svg/LogoutIconSvg'
+import LightModeSwitchIconSvg from '../Svg/LightModeSwitchIconSvg'
+import DarkModeSwitchIconSvg from '../Svg/DarkModeSwitchIconSvg'
 
-const QueraTaskManager = (): JSX.Element => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => {
+const SidebarPrimary = (): JSX.Element => {
+  const [darkMode, setDarkMode] = useState(false)
+  const toggleDarkMode = (): void => {
     setDarkMode(!darkMode)
   }
   return (
-    <section className="h-screen w-[340px] relative flex flex-col top-0 right-0 ml-auto bg-[#ffff] justify-between">
-      <div className="flex p-6 text-right flex-col justify-center items-center gap-y-[32px]">
+    <section className="relative right-0 top-0 ml-auto flex h-screen w-[340px] flex-col justify-between bg-[#ffff]">
+      <div className="flex flex-col items-center justify-center gap-y-[32px] p-6 text-right">
         {/* top text */}
         <div>
-          <p className="text-right text-cyan-primary text-headingl">
+          <p className="text-right text-headingl text-cyan-primary">
             کوئرا تسک منیجر
           </p>
         </div>
@@ -26,7 +26,7 @@ const QueraTaskManager = (): JSX.Element => {
         <div className="flex flex-col space-y-5">
           <div className="flex justify-between space-x-32">
             <ArrowDownIconSvg />
-            <p className="font-extrabold text-[16px]">ورک‌اسپیس‌ها</p>
+            <p className="text-[16px] font-extrabold">ورک‌اسپیس‌ها</p>
           </div>
           {/* --------------------------------------- */}
           {/* search input  */}
@@ -34,11 +34,11 @@ const QueraTaskManager = (): JSX.Element => {
             <div className="flex items-center">
               <input
                 type="text"
-                className="bg-[#F6F7F9] block w-[274px] h-[40px] py-4 px-10 ps-10 rounded-[4px] text-right placeholder:pr-2 placeholder:text-bodyxs placeholder:font-yekan placeholder:font-normal text-[#1E1E1E] rounded-[4px]"
+                className="block h-[40px] w-[274px] rounded-[4px] bg-[#F6F7F9] px-10 py-4 ps-10 text-right text-[#1E1E1E] placeholder:pr-2 placeholder:font-yekan placeholder:text-bodyxs placeholder:font-normal"
                 placeholder="جستجو کنید"
               />
               <div className="relative">
-                <div className="absolute inset-y-0 right-0 px-3 flex items-center ps-3 pointer-events-none">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 ps-3">
                   <SearchIconSvg />
                 </div>
               </div>
@@ -49,8 +49,8 @@ const QueraTaskManager = (): JSX.Element => {
           <div className="flex flex-col gap-[8px] space-y-[8px]">
             {/* Creating a new space */}
             <div className="flex flex-col">
-              <div className="w-full flex justify-center items-center bg-[#D3D3D3] space-x-1 rounded-[6px]">
-                <p className="text-[#1E1E1E] font-normal text-bodyxs">
+              <div className="flex w-full items-center justify-center space-x-1 rounded-[6px] bg-[#D3D3D3]">
+                <p className="text-bodyxs font-normal text-[#1E1E1E]">
                   ساختن اسپیس جدید
                 </p>
                 <AddSecondaryIconSvg />
@@ -62,43 +62,43 @@ const QueraTaskManager = (): JSX.Element => {
               {/* one list work */}
               <li>
                 <div className="flex justify-end space-x-2">
-                  <p className="font-medium text-[16px] text-right text-[#1E1E1E]">
+                  <p className="text-right text-[16px] font-medium text-[#1E1E1E]">
                     درس مدیریت پروژه
                   </p>
                   <div>
-                    <div className="bg-[#40C057] w-[20px] h-[20px] rounded-[4px]"></div>
+                    <div className="size-[20px] rounded-[4px] bg-[#40C057]"></div>
                   </div>
                 </div>
               </li>
               {/* two list work */}
-              <li className="flex flex-col space-y-[6px] gap-[6px]">
+              <li className="flex flex-col gap-[6px] space-y-[6px]">
                 <div className="flex justify-end space-x-2">
-                  <p className="font-medium text-[16px] text-right text-[#1E1E1E]">
+                  <p className="text-right text-[16px] font-medium text-[#1E1E1E]">
                     کارهای شخصی
                   </p>
                   <div>
-                    <div className="bg-[#FAB005] w-[20px] h-[20px] rounded-[4px]"></div>
+                    <div className="size-[20px] rounded-[4px] bg-[#FAB005]"></div>
                   </div>
                 </div>
                 {/* -------------------------------------START------------------------------------------- */}
                 {/* --------------------------- subcategory - کارهای شخصی -------------------------- */}
                 <li>
                   <div className="flex justify-end space-x-2">
-                    <p className="font-medium text-[16px] text-right text-[#1E1E1E]">
+                    <p className="text-right text-[16px] font-medium text-[#1E1E1E]">
                       پروژه اول
                     </p>
                     <div>
-                      <div className="w-[20px] h-[20px] rounded-[4px]"></div>
+                      <div className="size-[20px] rounded-[4px]"></div>
                     </div>
                   </div>
                 </li>
                 <li>
                   <div className="flex justify-end space-x-2">
-                    <p className="font-medium text-[16px] text-right text-[#1E1E1E]">
+                    <p className="text-right text-[16px] font-medium text-[#1E1E1E]">
                       پروژه دوم
                     </p>
                     <div>
-                      <div className="w-[20px] h-[20px] rounded-[4px]"></div>
+                      <div className="size-[20px] rounded-[4px]"></div>
                     </div>
                   </div>
                 </li>
@@ -108,28 +108,28 @@ const QueraTaskManager = (): JSX.Element => {
               {/* three  list work */}
               <li>
                 <div className="flex justify-end space-x-2">
-                  <p className="font-medium text-[16px] text-right text-[#1E1E1E]">
+                  <p className="text-right text-[16px] font-medium text-[#1E1E1E]">
                     درس کامپایلر
                   </p>
                   <div>
-                    <div className="bg-[#FA5252] w-[20px] h-[20px] rounded-[4px]"></div>
+                    <div className="size-[20px] rounded-[4px] bg-[#FA5252]"></div>
                   </div>
                 </div>
               </li>
               {/* --------------------------------------START------------------------------------------ */}
               {/* ----------!!!!!!!!!!!!!!!!-button new project - ساختن پروژه جدید !!!!!!!!!!!!!!!!-------------------------------------------- */}
-              <button className="w-[274px] hidden=[36px] border-[2px] border-[#208D8E] p-[4px] rounded-[8px] gap-[8px] text-[#208D8E] text-bodys text-center font-normal ">
+              <button className="hidden=[36px] w-[274px] gap-[8px] rounded-[8px] border-[2px] border-[#208D8E] p-[4px] text-center text-bodys font-normal text-[#208D8E] ">
                 ساختن پروژه جدید
               </button>
               {/* -----------------------------------------END--------------------------------------- */}
               {/* four  list work */}
               <li>
                 <div className="flex justify-end space-x-2">
-                  <p className="font-medium text-[16px] text-right text-[#1E1E1E]">
+                  <p className="text-right text-[16px] font-medium text-[#1E1E1E]">
                     درس طراحی الگوریتم
                   </p>
                   <div>
-                    <div className="bg-[#228BE6] w-[20px] h-[20px] rounded-[4px]"></div>
+                    <div className="size-[20px] rounded-[4px] bg-[#228BE6]"></div>
                   </div>
                 </div>
               </li>
@@ -140,29 +140,31 @@ const QueraTaskManager = (): JSX.Element => {
       </div>
       {/* ------------------------------ */}
       {/* account information */}
-      <div className="flex flex-col relative bottom-3 justify-end">
-        <div className="flex justify-end p-[16px] space-x-2">
-          <p className="font-medium text-[16px] text-right">نیلوفر موجودی</p>
-          <div className="h-[33px] w-[36px] rounded-[100px] pt-[9px] pb-[7px] px-[8px] bg-blue-secondary text-[#4C6EF5] font-normal text-bodyxs text-right">
+      <div className="relative bottom-3 flex flex-col justify-end">
+        <div className="flex justify-end space-x-2 p-[16px]">
+          <p className="text-right text-[16px] font-medium">نیلوفر موجودی</p>
+          <div className="h-[33px] w-[36px] rounded-[100px] bg-blue-secondary px-[8px] pb-[7px] pt-[9px] text-right text-bodyxs font-normal text-[#4C6EF5]">
             NM
           </div>
           {/* <LogoutIconSvg/> */}
         </div>
         {/* -------------------------------------- */}
         {/* Exit and toggle dark Light tame */}
-        <div className="flex justify-between p-[16px] space-x-2">
-          <div className="flex justify-center items-center transition-all ease-in-out delay-300 bg-blue-500">
+        <div className="flex justify-between space-x-2 p-[16px]">
+          <div className="flex items-center justify-center bg-blue-secondary transition-all delay-300 ease-in-out">
             <button onClick={toggleDarkMode}>
-              {darkMode ? (
+              {darkMode
+                ? (
                 <LightModeSwitchIconSvg />
-              ) : (
+                  )
+                : (
                 <DarkModeSwitchIconSvg />
-              )}
+                  )}
             </button>
           </div>
-          <div className="flex justify-center items-center space-x-2">
+          <div className="flex items-center justify-center space-x-2">
             <LogoutIconSvg />
-            <p className="text-[16px] font-medium text-right text-[#818181]">
+            <p className="text-right text-[16px] font-medium text-[#818181]">
               خروج
             </p>
           </div>
@@ -173,4 +175,4 @@ const QueraTaskManager = (): JSX.Element => {
   )
 }
 
-export default QueraTaskManager
+export default SidebarPrimary
