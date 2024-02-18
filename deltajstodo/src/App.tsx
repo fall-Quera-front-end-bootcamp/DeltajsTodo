c81dfd8a35119ef047a17778d6b15e2fb678604a
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2024-02-19 01:16:16 +0330

    need svg component import and chek font ...

--- deltajstodo/src/App.tsx
+++ deltajstodo/src/App.tsx
@@ -5,26 +5,20 @@ import Register from './pages/Register'
 import ForgetPassword from './pages/ForgetPassword'
 import ResetPassword from './pages/ResetPassword'
 import NotFound from './pages/NotFound'
-import MiniTaskCard from './components/TaskAndCheckBox/MiniTaskCard'
-import LargTaskCard from './components/TaskAndCheckBox/LargTaskCard'
 
 // eslint-disable-next-line @typescript-eslint/space-before-function-paren
 function App(): JSX.Element {
   return (
-    <div className='space-y-11 m-28'>
-    <MiniTaskCard/>
-    <LargTaskCard/>
-    </div>
-    // <>
-    //   <Routes>
-    //     <Route path="api/auth/login" element={<Login />} />
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="api/auth/register" element={<Register />} />
-    //     <Route path="api/auth/forgetPassword" element={<ForgetPassword />} />
-    //     <Route path="api/auth/resetPassword" element={<ResetPassword />} />
-    //     <Route path="*" element={<NotFound />} />
-    //   </Routes>
-    // </>
+    <>
+      <Routes>
+        <Route path="api/auth/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="api/auth/register" element={<Register />} />
+        <Route path="api/auth/forgetPassword" element={<ForgetPassword />} />
+        <Route path="api/auth/resetPassword" element={<ResetPassword />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 
