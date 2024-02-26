fb8617750a1502c7dafc4f29ba8885e52b99ea83
behrad afsharipour <48618902+behradahp@users.noreply.github.com>
2024-02-26 22:53:23 +0330

    Update Workspace.tsx

--- deltajstodo/src/pages/Workspace.tsx
+++ deltajstodo/src/pages/Workspace.tsx
@@ -1,15 +1,1 @@
-import { type FunctionComponent } from 'react'
-import TaskInformation from '../components/TaskInformation/TaskInformation'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface WorkSpaceProps {}
-
-const WorkSpace: FunctionComponent<WorkSpaceProps> = () => {
-  return (
-    <div className="flex justify-center items-center w-[100%] h-[100vh] bg-gray-dark">
-      <TaskInformation />
-    </div>
-  )
-}
-
-export default WorkSpace
