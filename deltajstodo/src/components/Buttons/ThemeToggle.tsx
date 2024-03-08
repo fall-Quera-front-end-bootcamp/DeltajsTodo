3f0df1aff586a3cc9ffa61fcad036f2bb9758d7a
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-03-08 17:30:54 +0330

    fix : add dark mode function to project

--- deltajstodo/src/components/Buttons/ThemeToggle.tsx
+++ deltajstodo/src/components/Buttons/ThemeToggle.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import { useState, type FunctionComponent } from 'react'
+import { useState, type FunctionComponent, useEffect } from 'react'
 import DarkModeSwitchIconSvg from '../Icons/DarkModeSwitchIconSvg'
 import LightModeSwitchIconSvg from '../Icons/LightModeSwitchIconSvg'
 
 interface ThemeToggleProps {}
 
 const ThemeToggle: FunctionComponent<ThemeToggleProps> = () => {
-  const [darkTheme, setDarkTheme] = useState(false)
+  const [darkTheme, setDarkTheme] = useState('light')
 
   const [insideDivPosition, setInsideDivPosition] = useState('left-[31px]')
   const [insideDivBg, setInsideDivBg] = useState('bg-[#FFFFFF]')
@@ -14,28 +14,34 @@ const ThemeToggle: FunctionComponent<ThemeToggleProps> = () => {
   const [lightSvgColor, setLightSvgColor] = useState('#262626')
   const [darkSvgColor, setDarkSvgColor] = useState('#F1F3F5')
 
-  const themeChanger = (): void => {
-    if (darkTheme) {
-      setInsideDivPosition('left-[31px]')
-      setInsideDivBg('bg-[#FFFFFF]')
-      setContainerDivBg('bg-[#F1F3F5]')
-      setLightSvgColor('#262626')
-      setDarkSvgColor('#F1F3F5')
-    } else {
+  useEffect(() => {
+    if (darkTheme === 'dark') {
       setInsideDivPosition('left-[3px]')
       setInsideDivBg('bg-[#868E96]')
       setContainerDivBg('bg-[#343A40]')
       setLightSvgColor('#343A40')
       setDarkSvgColor('white')
+      document.documentElement.classList.add('dark')
+    } else {
+      setInsideDivPosition('left-[31px]')
+      setInsideDivBg('bg-[#FFFFFF]')
+      setContainerDivBg('bg-[#F1F3F5]')
+      setLightSvgColor('#262626')
+      setDarkSvgColor('#F1F3F5')
+      document.documentElement.classList.remove('dark')
     }
+  }, [darkTheme])
 
-    setDarkTheme((prev) => !prev)
+  const handleThemeSwitch = (): void => {
+    setDarkTheme(darkTheme === 'dark' ? 'light' : 'dark')
   }
+  console.log(darkTheme)
+
   return (
     <div
       dir="rtl"
       className={`relative flex h-[36px] w-[64px] items-center justify-around rounded-lg ${containerDivBg} cursor-pointer p-[3px] transition-all duration-300`}
-      onClick={themeChanger}
+      onClick={handleThemeSwitch}
     >
       <div
         className={`absolute ${insideDivPosition} size-[30px] rounded-[5px] ${insideDivBg} transition-all duration-300`}
