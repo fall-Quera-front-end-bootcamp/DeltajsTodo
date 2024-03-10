b26e40ed5324a6b339ffd7eb30c4ad0255c8723f
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-03-11 02:20:18 +0330

    fix : constant

--- deltajstodo/src/constants/index.ts
+++ deltajstodo/src/constants/index.ts
@@ -8,6 +8,38 @@ export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 export const CALENDAR_SIZE = 42
 
+export const hexColors = {
+  'gray-secondary': '#F1F3F5',
+  'gray-primary': '#868E96',
+  'gray-dark': '#343A40',
+  'red-secondary': '#FFE3E3',
+  'red-primary': '#FA5252',
+  'pink-secondary': '#FFDEEB',
+  'pink-primary': '#E64980',
+  'grape-secondary': '#F3D9FA',
+  'grape-primary': '#BE4BDB',
+  'violet-secondary': '#E5DBFF',
+  'violet-primary': '#7950F2',
+  'indigo-secondary': '#DBE4FF',
+  'indigo-primary': '#4C6EF5',
+  'blue-secondary': '#D0EBFF',
+  'blue-primary': '#228BE6',
+  'cyan-secondary': '#C5F6FA',
+  'cyan-primary': '#15AABF',
+  'teal-secondary': '#C3FAE8',
+  'teal-primary': '#12B886',
+  'brand-secondary': '#C2F7FA',
+  'brand-primary': '#208D8E',
+  'green-secondary': '#D3F9D8',
+  'green-primary': '#40C057',
+  'lime-secondary': '#E9FAC8',
+  'lime-primary': '#82C91E',
+  'yellow-secondary': '#FFF3BF',
+  'yellow-primary': '#FAB005',
+  'orange-secondary': '#FFE8CC',
+  'orange-primary': '#FD7E14'
+}
+
 export const dayOfWeek = [
   'شنبه',
   'یکشنبه',
