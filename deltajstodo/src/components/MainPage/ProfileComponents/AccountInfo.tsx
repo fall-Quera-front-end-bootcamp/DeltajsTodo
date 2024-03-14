e522f15d5a0c19a6fb1d6b31c23c3c2ee4e5dbc6
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-03-14 23:44:13 +0330

    fix : fix confilcts

---
+++ deltajstodo/src/components/MainPage/ProfileComponents/AccountInfo.tsx
@@ -0,0 +1,225 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { useEffect, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { motion } from 'framer-motion'
+
+import { store } from '../../../app/store'
+import {
+  useGetUserQuery,
+  useUpdateInfoMutation,
+  useUpdatePassMutation
+} from '../../../features/users/usersInteractionApiSlice'
+
+import Loading from '../../Common/Loading/Loading'
+import Input from '../../Common/Input/Input'
+
+interface AccountInfoProps {
+  messageFunction: Function
+}
+
+const AccountInfo = ({ messageFunction }: AccountInfoProps) => {
+  const id = store.getState().auth.user?.user_id
+
+  const getUser = useGetUserQuery(id)
+  const [UpdateInfo] = useUpdateInfoMutation()
+  const [UpdatePass] = useUpdatePassMutation()
+  const methods = useForm()
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [isSending, setIsSending] = useState(false)
+  const [userData, setUserData] = useState(['', ''])
+
+  const accountEmailProps = {
+    name: 'account_email',
+    validation: {
+      required: {
+        value: true,
+        message: 'لطفا ایمیل خود را وارد کنید'
+      }
+    },
+    id: 'account_email',
+    label: 'ایمیل',
+    type: 'email',
+    initialValue: `${userData[0]}`
+  }
+
+  const accountUserNameProps = {
+    name: 'account_user_name',
+    validation: {
+      required: {
+        value: true,
+        message: 'لطفا نام کاربری خود را وارد کنید'
+      }
+    },
+    id: 'account_user_name',
+    label: 'نام کاربری',
+    type: 'text',
+    initialValue: `${userData[1]}`,
+    autocomplete: 'off'
+  }
+
+  const accountCurrentPasswordProps = {
+    name: 'account_current_pass',
+    validation: {
+      required: {
+        value: false,
+        message: 'لطفا رمز عبور خود را وارد کنید'
+      }
+    },
+    id: 'account_current_pass',
+    label: 'رمز عبور فعلی',
+    type: 'password',
+    autocomplete: 'new-password'
+  }
+
+  const accountNewPasswordProps = {
+    name: 'account_new_pass',
+    validation: {
+      required: {
+        value: false,
+        message: 'لطفا رمز عبور خود را وارد کنید'
+      }
+    },
+    id: 'account_new_pass',
+    label: 'رمز عبور جدید',
+    type: 'password'
+  }
+
+  const accountRepeatedNewPasswordProps = {
+    name: 'account_repeated_new_pass',
+    validation: {
+      required: {
+        value: false,
+        message: 'لطفا رمز عبور خود را وارد کنید'
+      }
+    },
+    id: 'account_repeated_new_pass',
+    label: 'تکرار رمز عبور جدید',
+    type: 'password'
+  }
+
+  useEffect(() => {
+    if (getUser.status === 'fulfilled') {
+      setUserData([getUser.data.email, getUser.data.username])
+      setIsLoading(false)
+    }
+  }, [getUser])
+
+  const onSubmit = methods.handleSubmit((data) => {
+    if (
+      !data.account_current_pass &&
+      !data.account_new_pass &&
+      !data.account_repeated_new_pass
+    ) {
+      setIsSending(true)
+
+      const updateInfo: {
+        id: number | undefined
+        email: string
+        username: string
+      } = {
+        id: id,
+        email: data.account_email,
+        username: data.account_user_name
+      }
+      handleInfoChange(updateInfo)
+    } else if (
+      data.account_current_pass &&
+      data.account_new_pass &&
+      data.account_repeated_new_pass
+    ) {
+      setIsSending(true)
+      const updatePass = {
+        old_password: data.account_current_pass,
+        new_password: data.account_new_pass,
+        new_password1: data.account_repeated_new_pass
+      }
+      handlePassChange(updatePass)
+    } else {
+      messageFunction('فیلد‌های رمز عبور خالی است', 'fail')
+    }
+  })
+
+  const handleInfoChange = async (data: {
+    id: number | undefined
+    email: string
+    username: string
+  }): Promise<void> => {
+    try {
+      await UpdateInfo(data).unwrap()
+      messageFunction('اطلاعات با موفقیت تغییر کرد', 'success')
+    } catch (err: any) {
+      messageFunction('مشکلی در تغییر اطلاعات به وجود آمده', 'fail')
+      methods.reset
+    }
+    setIsSending(false)
+  }
+
+  const handlePassChange = async (data: {
+    old_password: string
+    new_password: string
+    new_password1: string
+  }): Promise<void> => {
+    try {
+      await UpdatePass(data).unwrap()
+      messageFunction('رمز با موفقیت تغییر کرد', 'success')
+    } catch (err: any) {
+      messageFunction('تغییر رمز عبور موفقیت آمیز نبود', 'fail')
+      methods.reset
+    }
+    setIsSending(false)
+  }
+
+  if (isLoading) {
+    return (
+      <Loading classNames="bg-brand-primary rounded-[30px]">
+        <span className="text-white">Loading</span>
+        <></>
+      </Loading>
+    )
+  } else {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        exit={{
+          opacity: 1,
+          transition: { duration: 1 }
+        }}
+        onSubmit={onSubmit}
+        className="flex flex-col gap-8"
+      >
+        <div className="flex flex-col gap-4">
+          <FormProvider {...methods}>
+            <motion.form
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+              exit={{
+                opacity: 1,
+                transition: { duration: 1 }
+              }}
+              onSubmit={(e) => e.preventDefault()}
+              className="flex flex-col gap-10"
+            >
+              <div className="w-[354px] flex flex-col gap-4">
+                <Input {...accountEmailProps} />
+                <Input {...accountUserNameProps} />
+                <Input {...accountCurrentPasswordProps} />
+                <Input {...accountNewPasswordProps} />
+                <Input {...accountRepeatedNewPasswordProps} />
+              </div>
+
+              <button className="flex justify-center w-[354px] rounded-md py-[8px] px-[12px] bg-brand-primary text-white font-extrabold text-[14px] leading-[19.73px]">
+                {isSending ? <Loading /> : <span>ثبت تغییرات</span>}
+              </button>
+            </motion.form>
+          </FormProvider>
+        </div>
+      </motion.div>
+    )
+  }
+}
+
+export default AccountInfo
--- deltajstodo/src/pages/MainPages/Profile.tsx
+++ deltajstodo/src/pages/MainPages/Profile.tsx
@@ -1,1 +1,79 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { useState, type FunctionComponent } from 'react'
+import ProfileInfo from '../../components/MainPage/ProfileComponents/ProfileInfo'
+import AccountInfo from '../../components/MainPage/ProfileComponents/AccountInfo'
+import Setting from '../../components/MainPage/ProfileComponents/Setting'
+import ProfileSideBar from '../../components/MainPage/ProfileComponents/ProfileSideBar'
+import Message from '../../components/Common/Message/Message'
 
+interface ProfileProps {}
+
+const Profile: FunctionComponent<ProfileProps> = () => {
+  const [section, setSection] = useState('info')
+  const [title, setTitle] = useState('اطلاعات فردی')
+  const [messages, setMessages] = useState<
+    Array<{ text: string; type: string }>
+  >([])
+
+  const setMessage = (text: string, type: string) => {
+    setMessages((prev) => [...prev, { text: text, type: type }])
+    const remove = setTimeout(() => {
+      setMessages([])
+    }, 5000)
+    return () => clearTimeout(remove)
+  }
+
+  const changeSection = (e: any) => {
+    setSection(e.target.ariaLabel)
+    switch (e.target.ariaLabel) {
+      case 'info':
+        setTitle('اطلاعات فردی')
+        break
+      case 'account':
+        setTitle('اطلاعات حساب')
+        break
+      default:
+        setTitle('تنظیمات')
+    }
+  }
+
+  const sectionRendering = () => {
+    switch (section) {
+      case 'info':
+        return <ProfileInfo messageFunction={setMessage} />
+      case 'account':
+        return <AccountInfo messageFunction={setMessage} />
+      default:
+        return <Setting messageFunction={setMessage} />
+    }
+  }
+
+  return (
+    <div dir="rtl" className={`flex w-[100%] h-[100vh]`}>
+      <div className="fixed top-3 left-[50%] translate-x-[-50%] z-10">
+        {messages.map((item, index) => {
+          if (index < messages.length / 2) {
+            return <Message key={index} text={item.text} type={item.type} />
+          }
+        })}
+      </div>
+      {/* sidebar */}
+      <div className="w-[340px] h-[100%]">
+        <ProfileSideBar onClickFunction={changeSection} selected={section} />
+      </div>
+
+      {/* main content */}
+      <div className="relative flex-grow-[2] py-[170px] px-[58px] overflow-y-scroll no-scrollbar">
+        <div className={`flex flex-col gap-8 transition-[height] duration-700`}>
+          <span className="font-bold text-[31px] leading-[43.68px] animate-fadein">
+            {title}
+          </span>
+
+          {sectionRendering()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
